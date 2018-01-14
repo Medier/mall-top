bf90260086b1672c84d842659f7fe97d7da15781
Medier <medier_luo@163.com>
2018-01-14 20:36:14 +0800

    modify in github

--- mall-top-microservice-item/src/main/java/com/mall/item/microservice/service/ItemService.java
+++ mall-top-microservice-item/src/main/java/com/mall/item/microservice/service/ItemService.java
@@ -24,6 +24,8 @@ public class ItemService {
         ITEM_MAP.put(4L, new Item(4L, "商品标题4", "商品图片4", "商品描述4", 400L));
         ITEM_MAP.put(5L, new Item(5L, "商品标题5", "商品图片5", "商品描述5", 500L));
         ITEM_MAP.put(6L, new Item(6L, "商品标题6", "商品图片6", "商品描述6", 600L));
+        ITEM_MAP.put(7L, new Item(7L, "商品标题7", "商品图片7", "商品描述7", 700L));
+        ITEM_MAP.put(8L, new Item(8L, "商品标题8", "商品图片8", "商品描述8", 800L));
 
     }
 
